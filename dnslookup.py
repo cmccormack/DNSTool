6fcb9fe8b746f74b969dea91bb245036c59a1a1a
Christopher McCormack <mccormack.christopher@gmail.com>
2015-10-11 08:35:06 -0700

    Added Google/OpenDNS nameservers as defaults

--- dnslookup.py
+++ dnslookup.py
@@ -1,29 +1,22 @@
 import dns.resolver
 import dns.reversename
 import ipaddress
-import sys
 import argparse
 
-ipcdns = ['148.173.250.189', '148.173.129.21']
-gsodns = ['148.173.250.45', '148.173.250.189']
-
-domain_servers = dict(
-    americas_east = ['10.21.12.85', '10.21.12.86', '148.173.250.27'], 
-    americas_west = ['148.173.250.201', '148.173.250.27', '10.21.12.85'], 
-    emea_east = ['10.21.32.10', '164.12.33.23', '10.21.12.86'], 
-    emea_west = ['164.12.33.23', '10.21.32.10', '10.21.12.86'], 
-    lac = ['148.173.52.238', '10.21.1.85', '148.173.250.27'], 
-    japa_south = ['148.172.200.46', '148.172.160.50', '148.172.134.186'], 
-    japa_north = ['148.172.150.60', '148.172.134.186', '148.172.200.46']
-)
-
-print domain_servers
-
 my_resolver = dns.resolver.Resolver()
-my_resolver.nameservers = ipcdns
+
+# Use Google and OpenDNS NameServers by default
+my_resolver.nameservers = [
+    '8.8.8.8', 
+    '208.67.222.222', 
+    '8.8.4.4', 
+    '208.67.220.220'
+]
+
 my_resolver.timeout = 1
 
-MAX_IP_LEN = len('XXX.XXX.XXX.XXX')
+# Max length of four dot separated octets (e.g. 255.255.255.255)
+MAX_IP_LEN = 15
 
 def dns_reverse_lookup(subnet):
 
@@ -36,11 +29,12 @@ def dns_reverse_lookup(subnet):
                 ip, MAX_IP_LEN, queryresult)
 
         except dns.resolver.NXDOMAIN:
-            print '[-] IP: {0: <{1}}  No Record Found (NXDOMAIN)'.format(
+            print '[-] IP: {0: <{1}}  [No Record Found (NXDOMAIN)]'.format(
                 ip, MAX_IP_LEN)
 
         except dns.resolver.Timeout:
             print 'Timed out while resolving {0}'.format(ip)
+    print 
 
 
 def dns_lookup(name, longest=40):
@@ -50,16 +44,19 @@ def dns_lookup(name, longest=40):
         print '[+] IP: {0: <{1}}  Hostname: {2}'.format(
             queryresult.rrset.to_text().split()[-1], MAX_IP_LEN, name)
     except:
-        pass
+        print '[-] IP: {0: <{1}}  Hostname: {2}  [No Record Found (NXDOMAIN)]'.format(
+                '', MAX_IP_LEN, name)
 
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Perform DNS Queries.")
-    parser.add_argument('-r', '--reverse', action='store_true', default=False)
-    parser.add_argument('host', metavar='HOST', action='store', help='Hostname or IP Address/IP Subnet')
-
+    parser.add_argument('-r', '--reverse', action='store_true', default=False,
+        help='Perform a reverse lookup on an IP address.  \
+        This is the default behavior when an IP address is provided')
+    parser.add_argument('host', metavar='HOST', action='store', 
+        help='Hostname or IP Address/IP Subnet')
 
     args = parser.parse_args()
 
@@ -68,10 +65,16 @@ if __name__ == '__main__':
             subnet = ipaddress.ip_network(args.host, strict=False)
             dns_reverse_lookup(subnet)
         except:
-            print 'Invalid IP Address/Subnet [{0}]'.format(args.host)
+            print 'Invalid IP Address/Subnet [{0}]\n'.format(args.host)
     else:
         try:
             subnet = ipaddress.ip_network(args.host, strict=False)
             dns_reverse_lookup(subnet)
         except:
             dns_lookup(args.host)
+
+
+
+
+
+
